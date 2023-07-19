632f891ebc3fa11aa0f5d3279bccdd85cd1bd756
erenkonuk <136901974+erenkonuk@users.noreply.github.com>
2023-07-19 10:10:13 +0300

    Add project files.

---
+++ C# Program to Divide Sequence into Groups using LINQ/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var seq = Enumerable.Range(100, 100).Select(x => x / 10f);
+        var grps = from x in seq.Select((i, j) => new { i, Grp = j / 10 })
+                   group x.i by x.Grp into y
+                   select new { Min = y.Min(), Max = y.Max() };
+        foreach (var grp in grps)
+            Console.WriteLine("Min: " + grp.Min + " Max:" + grp.Max);
+        Console.ReadLine();
+    }
+}
+
